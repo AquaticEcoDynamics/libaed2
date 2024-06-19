beab37989625874dc9137ae2c3b1c637c63abd71
Casper Boon <casper.boon@uwa>
2024-06-19 14:45:28 +0800

    added functionality to dummy for testing

--- src/aed2_dummy.F90
+++ src/aed2_dummy.F90
@@ -68,6 +68,8 @@ MODULE aed2_dummy
       INTEGER :: id_sine, id_vsine
       INTEGER,ALLOCATABLE :: id_dummy_v(:), id_dummy_dv(:),           &
                              id_dummy_sv(:), id_dummy_dsv(:)
+      AED_REAL,ALLOCATABLE :: dm_max(:), dm_min(:)
+      AED_REAL,ALLOCATABLE :: dm_smax(:), dm_smin(:)
 
      CONTAINS
          PROCEDURE :: define            => aed2_define_dummy
@@ -76,8 +78,12 @@ MODULE aed2_dummy
    END TYPE
 
 ! MODULE GLOBALS
-   INTEGER :: diag_level = 10
    AED_REAL :: today = 1.
+   INTEGER  :: diag_level = 10                ! 0 = no diagnostic outputs
+                                              ! 1 = basic diagnostic outputs
+                                              ! 2 = flux rates, and supporitng
+                                              ! 3 = other metrics
+                                              !10 = all debug & checking outputs
 
 !===============================================================================
 CONTAINS
@@ -103,32 +109,34 @@ SUBROUTINE aed2_define_dummy(data, namlst)
 
    CHARACTER(len=4),POINTER :: prefix => null()
 
-!  %% NAMELIST
-   CHARACTER(len=40) :: dm_vars(100)
-   AED_REAL          :: dm_max(100)
-   AED_REAL          :: dm_min(100)
-   AED_REAL          :: dm_init(100)
-   CHARACTER(len=40) :: dm_dvars(100)
-   CHARACTER(len=40) :: dm_svars(100)
-   AED_REAL          :: dm_smax(100)
-   AED_REAL          :: dm_smin(100)
-   AED_REAL          :: dm_sinit(100)
-   CHARACTER(len=40) :: dm_dsvars(100)
-!  %% END NAMELIST
+!  %% NAMELIST   %%  /aed2_dummy/
+!  %% Last Checked 20/08/2021
+   CHARACTER(len=40) :: dm_vars(100) = ''
+   AED_REAL          :: dm_max(100) = NaN_
+   AED_REAL          :: dm_min(100) = NaN_
+   AED_REAL          :: dm_init(100) = 0.
+   CHARACTER(len=40) :: dm_dvars(100) = ''
+   CHARACTER(len=40) :: dm_svars(100) = ''
+   AED_REAL          :: dm_smax(100) = NaN_
+   AED_REAL          :: dm_smin(100) = NaN_
+   AED_REAL          :: dm_sinit(100) = 0.
+   CHARACTER(len=40) :: dm_dsvars(100) = ''
+! %% From Module Globals
+!  INTEGER  :: diag_level = 10                ! 0 = no diagnostic outputs
+!                                             ! 1 = basic diagnostic outputs
+!                                             ! 2 = flux rates, and supporitng
+!                                             ! 3 = other metrics
+!                                             !10 = all debug & checking outputs
+!  %% END NAMELIST   %%  /aed2_dummy/
 
    NAMELIST /aed2_dummy/ dm_vars, dm_max, dm_min, dm_init,             &
                          dm_dvars,                                     &
                          dm_svars, dm_smax, dm_smin, dm_sinit,         &
-                         dm_dsvars
+                         dm_dsvars, diag_level
 !
 !-------------------------------------------------------------------------------
 !BEGIN
    print *,"        aed2_dummy initialization"
-
-   dm_vars = ''    ; dm_dvars = ''
-   dm_max = NaN_   ; dm_min = NaN_   ; dm_init = 0
-   dm_svars = ''   ; dm_dsvars = ''
-   dm_smax = NaN_  ; dm_smin = NaN_  ; dm_sinit = 0
 
    num_v = 0 ; num_dv = 0 ; num_sv = 0 ; num_dsv = 0
 
@@ -146,6 +154,9 @@ SUBROUTINE aed2_define_dummy(data, namlst)
    ALLOCATE(data%id_dummy_sv(num_sv))
    ALLOCATE(data%id_dummy_dsv(num_dsv))
 
+   ALLOCATE(data%dm_min(num_v))   ; ALLOCATE(data%dm_max(num_v))
+   ALLOCATE(data%dm_smin(num_sv)) ; ALLOCATE(data%dm_smax(num_sv))
+
    data%num_v   = num_v
    data%num_dv  = num_dv
    data%num_sv  = num_sv
@@ -156,10 +167,14 @@ SUBROUTINE aed2_define_dummy(data, namlst)
    ! Register state variables
    DO i=1,data%num_v
       data%id_dummy_v(i) = aed2_define_variable(dm_vars(i), '', '', dm_init(i), dm_min(i), dm_max(i), 0.)
+      data%dm_min(i) = dm_min(i)
+      data%dm_max(i) = dm_max(i)
    ENDDO
 
    DO i=1,data%num_sv
       data%id_dummy_sv(i) = aed2_define_sheet_variable(dm_svars(i), '', '', dm_sinit(i), dm_smin(i), dm_smax(i), .FALSE.)
+      data%dm_smin(i) = dm_smin(i)
+      data%dm_smax(i) = dm_smax(i)
    ENDDO
 
    DO i=1,data%num_dv
@@ -172,7 +187,6 @@ SUBROUTINE aed2_define_dummy(data, namlst)
 
    data%id_vsine = aed2_define_diag_variable('DUM_vol_sine', 'no units', 'DBG volume sine between 0.0 and 1.0')
    data%id_sine = aed2_define_sheet_diag_variable('DUM_sine', 'no units', 'DBG sine wave between 0.0 and 1.0', .FALSE.)
-
 END SUBROUTINE aed2_define_dummy
 !+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
@@ -186,13 +200,20 @@ SUBROUTINE aed2_calculate_dummy(data,column,layer_idx)
    INTEGER,INTENT(in) :: layer_idx
 !
 !LOCALS
-!  INTEGER  :: i, count
-!  AED_REAL :: val, tot
+   INTEGER  :: i
+   AED_REAL :: scale, offs
 
 !-------------------------------------------------------------------------------
 !BEGIN
    _DIAG_VAR_(data%id_vsine) = &
         (sin(MOD((today+(layer_idx-1)*10.),365.)/365. * 2 * 3.1415) * 0.5) + 0.5
+
+   DO i=1,data%num_v
+      scale = (data%dm_max(i) - data%dm_min(i)) / 2.
+      offs = data%dm_min(i) + scale
+      _STATE_VAR_(data%id_dummy_v(i)) = &
+        (sin(MOD((today+(layer_idx-1)*10.),365.)/365. * 2 * 3.1415) * scale) + offs
+   ENDDO
 END SUBROUTINE aed2_calculate_dummy
 !+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
@@ -206,21 +227,23 @@ SUBROUTINE aed2_calculate_benthic_dummy(data,column,layer_idx)
    INTEGER,INTENT(in) :: layer_idx
 !
 !LOCALS
-   ! Environment
-!  AED_REAL :: temp
-
-   ! State
-!  AED_REAL :: ss, bottom_stress, matz
+   INTEGER :: i
+   AED_REAL :: scale, offs
 
 !-------------------------------------------------------------------------------
 !BEGIN
-
    IF (layer_idx .EQ. 1) today = today + 1.0/36.5
+
    _DIAG_VAR_S_(data%id_sine) = &
         (sin(MOD((today+(layer_idx-1)*10.),365.)/365. * 2 * 3.1415) * 0.5) + 0.5
 
+   DO i=1,data%num_sv
+      scale = (data%dm_smax(i) - data%dm_smin(i)) / 2.
+      offs = data%dm_smin(i) + scale
+      _STATE_VAR_S_(data%id_dummy_sv(i)) = &
+        (sin(MOD((today+(layer_idx-1)*10.),365.)/365. * 2 * 3.1415) * scale) + offs
+   ENDDO
 END SUBROUTINE aed2_calculate_benthic_dummy
 !+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
 
 END MODULE aed2_dummy
